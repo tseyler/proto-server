de49f45d9b4e6c48feaa8a5c02b1b2cebceb2c86
Terry Seyler <taseyler@gmail.com>
2015-07-23 08:06:45 -0600

    Makefile changes to accommodate Eclipse.

--- net_protocol/include/http/http_response_message.hpp
+++ net_protocol/include/http/http_response_message.hpp
@@ -19,30 +19,30 @@ namespace proto_http
     {
     public:
 
-	http_response_message();
-	http_response_message(const http_status_code& status_code);
-	http_response_message(const http_response_message& copy);
-	http_response_message& operator = (const http_response_message& rhs);
-	virtual void clone(const http_response_message& copy);
+		http_response_message();
+		http_response_message(const http_status_code& status_code);
+		http_response_message(const http_response_message& copy);
+		http_response_message& operator = (const http_response_message& rhs);
+		virtual void clone(const http_response_message& copy);
 
-	http_status_code status_code(void) const;
-	http_status_code& get_status_code(void);
+		http_status_code status_code(void) const;
+		http_status_code& get_status_code(void);
 
-	// http_message
-	virtual std::string http_version(void) const;
-	virtual void http_version(const std::string& version);
-	virtual http_headers headers(void) const;
-	virtual http_headers& get_headers(void);
-	virtual std::string status_line(void) const;
-	virtual std::string header_to_string(void) const;
-	virtual std::string to_string(void) const;
-	virtual net_data body(void) const;
-	virtual net_data& get_body(void);
-	virtual net_data to_net_data(void) const;
+		// http_message
+		virtual std::string http_version(void) const;
+		virtual void http_version(const std::string& version);
+		virtual http_headers headers(void) const;
+		virtual http_headers& get_headers(void);
+		virtual std::string status_line(void) const;
+		virtual std::string header_to_string(void) const;
+		virtual std::string to_string(void) const;
+		virtual net_data body(void) const;
+		virtual net_data& get_body(void);
+		virtual net_data to_net_data(void) const;
 	
     private:
 
-	http_status_code status_code_;
+		http_status_code status_code_;
     };
 
 }
