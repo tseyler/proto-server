f43239e46823965d4244bc31def7c76878adb71a
Terry Seyler <tseyler@control4.com>
2016-01-14 10:29:40 -0700

    Linking in resiprocate static libs.  Added file globbing to cmake.

--- make/definitions.cmake
+++ make/definitions.cmake
@@ -25,7 +25,13 @@ set(SIPUSERAGENT_INC_DIR ${CPP_PROJ_DIR}/sipuseragent/include)
 set(RESIP_VERSION 1.10.1)
 # resiprocate path
 set(RESIP_DIR $ENV{HOME}/resiprocate-${RESIP_VERSION})
+# paths to resip libs
+set(RESIP_DUM_LIB_DIR ${RESIP_DIR}/resip/dum/.libs)
+set(RESIP_STACK_LIB_DIR ${RESIP_DIR}/resip/stack/.libs)
+set(RESIP_RUTIL_LIB_DIR ${RESIP_DIR}/rutil/.libs)
+set(RESIP_LIB_DIR ${RESIP_DUM_LIB_DIR} ${RESIP_STACK_LIB_DIR} ${RESIP_RUTIL_LIB_DIR})
 
+file(GLOB GLOB_SRC_FILES ${CMAKE_BINARY_DIR}/src/*.cpp)
 
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${LIB_DIR}) 
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${LIB_DIR})
