724818852adcc9d36748b345f71ba2f605b91e2b
Terry Seyler <taseyler@gmail.com>
2016-01-25 12:39:12 -0700

    Changed name of io to pipeline.

--- proto_server/samples/servers/proto_echo_server/src/proto_echo_pipeline.cpp
+++ proto_server/samples/servers/proto_echo_server/src/proto_echo_pipeline.cpp
@@ -2,7 +2,7 @@
 // Created by tseyler on 1/4/16.
 //
 
-#include "proto_echo_io.hpp"
+#include "proto_echo_pipeline.hpp"
 
 void
 proto_echo_pipeline::ps_pipeline(const proto_net_in_data& req_data, proto_net_out_data& res_data)
