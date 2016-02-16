2c3c20d0b58683fab82f5399f0e82bc60ee67b89
Terry Seyler <taseyler@gmail.com>
2016-02-16 08:10:50 -0700

    Updated documentation.

--- proto_server/core/proto_service.hpp
+++ proto_server/core/proto_service.hpp
@@ -8,7 +8,8 @@
 namespace proto_net
 {
     /**
-     * proto_service: Pure abstract class for clients and servers
+     * \brief proto_service: Pure abstract class for clients and servers.
+     * The proto_service class is implemented by the base classes proto_server and proto_client.
      */
     class proto_service
     {
