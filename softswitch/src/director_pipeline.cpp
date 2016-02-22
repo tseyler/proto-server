08cf8011ccfa18c49a630b13e61f9e0e0a86ecf6
Terry Seyler <tseyler@control4.com>
2016-02-22 13:08:09 -0700

    Added in cmdline param for director address

--- softswitch/src/director_pipeline.cpp
+++ softswitch/src/director_pipeline.cpp
@@ -24,11 +24,10 @@ director_pipeline::ps_pipe_in(proto_net_in_data& in_data)
 void
 director_pipeline::ps_pipe_out(proto_net_out_data& out_data)
 {
-    proto_tcp_upstream_pipeline::ps_pipe_out(out_data);
-
     boost::unique_lock<boost::mutex> scoped_lock(mutex_);
     std::cout << "Director Pipeline: Received Data = " << out_data << std::endl;
     std::cout << " <========= Forwarding upstream to server." << std::endl;
-    std::cout << "[Round trip complete]" << std::endl;
+
+    proto_tcp_upstream_pipeline::ps_pipe_out(out_data);
 }
 
