1988ec743a406e8a15657a51363308613550bea6
Terry Seyler <tseyler@control4.com>
2016-02-24 16:35:13 -0700

    Added text client factory.

---
+++ proto_server/core/factory/proto_service_factory.hpp
@@ -0,0 +1,24 @@
+//
+// Created by tseyler on 2/24/16.
+//
+
+#ifndef PROTO_SERVICE_FACTORY_HPP_
+#define PROTO_SERVICE_FACTORY_HPP_
+
+#include <core/proto_service.hpp>
+
+namespace proto_net
+{
+    namespace factory
+    {
+        class proto_service_factory
+        {
+        public:
+
+            virtual proto_service_ptr ps_factory_create(bool start_on_creation = true) = 0;
+        };
+
+    }
+}
+
+#endif //PROTO_SERVICE_FACTORY_HPP_
