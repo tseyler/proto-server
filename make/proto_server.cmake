387ddb4b24c78bbaf86bf9ebcb0d2423a481cfa3
Terry Seyler <taseyler@gmail.com>
2016-02-28 20:16:41 -0700

    Added in cmake code to create shared library by uncommenting set(BUILD_SHARED_LIBS ON).  Added in post build message that the build was completed on the curent version of proto-server.

--- make/proto_server.cmake
+++ make/proto_server.cmake
@@ -1,6 +1,9 @@
 
 include(${MAKE_DIR}/definitions.cmake)
 
+# To build a shared library version of proto-server uncomment the following:
+#set(BUILD_SHARED_LIBS ON)
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++11")
 
 set(CORE_DIR ${PROTO_SERVER_DIR}/core)
@@ -29,5 +32,12 @@ set(INCLUDE_DIRS
 )
 
 include_directories(${INCLUDE_DIRS})
-
+		 
 add_library(proto_server ${SOURCE_FILES})
+
+#build messages
+set(PROTO_SERVER_VERSION "proto-server-0.1.0")
+add_custom_command(TARGET proto_server POST_BUILD
+                   COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --cyan
+                   "Build completed: ${PROTO_SERVER_VERSION}")
+
