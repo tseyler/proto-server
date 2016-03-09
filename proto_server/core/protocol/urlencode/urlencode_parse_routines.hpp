a3a424be0140ee926a958608db1856a8ff20e2ad
Terry Seyler <tseyler@control4.com>
2016-03-09 16:29:31 -0700

    Added more urlencode code.

--- proto_server/core/protocol/urlencode/urlencode_parse_routines.hpp
+++ proto_server/core/protocol/urlencode/urlencode_parse_routines.hpp
@@ -6,8 +6,9 @@
 #define URLENCODE_PARSE_ROUTINES_HPP_
 
 #include <string>
-#include <vector>
-#include <core/data/proto_net_string_data.hpp>
+//#include <vector>
+#include <map>
+//#include <core/data/proto_net_string_data.hpp>
 #include <core/protocol/net_parse_routines.hpp>
 
 namespace proto_net
@@ -16,8 +17,14 @@ namespace proto_net
     {
         namespace urlencode
         {
+            typedef std::map<std::string, std::string> urlencode_key_values;
+            typedef urlencode_key_values::iterator urlencode_key_values_iterator;
+            typedef std::pair<std::string, std::string> urlencode_key_value_pair;
+
             // tokenize routines
-            size_t tokenize_urlencode_line(const std::string line, matches_t &tokens);
+            size_t tokenize_urlencode_line(const std::string line, matches_t& tokens);
+
+            size_t tokenize_urlencode_key_value(matches_t& tokens, urlencode_key_values& key_values);
         }
     }
 }
